8b5981bf0afab08ed36b321e1645ed3036675e4f
David Lippman <drlippman@yahoo.com>
2014-07-02 10:56:16 -0700

    Add additional metadata to epub to QTI

--- QTI/OSepubQTI.php
+++ QTI/OSepubQTI.php
@@ -7,25 +7,76 @@
 // GPL License
 
 require("phpQuery-onefile.php");
+$meta = array();
 
+//Change this section
 $dir = 'bio';
 $filelevel = "section";  //or "chapter" or "question"
-$keywords = "OpenStax Concepts of Biology";
 
-function startqti($ident,$keywd) {
+$meta['book'] = 'Concepts of Biology';
+$meta['org'] = 'OpenStax';
+$meta['license'] = 'Creative Commons Attribution 3.0 Unported License';
+$meta['licenseurl'] = 'http://creativecommons.org/licenses/by/3.0/';
+$titleprefix = 'OpenStax Concepts of Biology';
+
+
+//end changes
+$assessuniq = uniqid();
+
+function startqti() {
+	global $meta,$assessuniq;
 	$c = '<?xml version="1.0" encoding="UTF-8"?>
 <questestinterop xmlns="http://www.imsglobal.org/xsd/ims_qtiasiv1p2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/ims_qtiasiv1p2 http://www.imsglobal.org/xsd/ims_qtiasiv1p2p1.xsd">
-  <assessment ident="'.$ident.'" title="'.$keywd.'">
+  <assessment ident="'.$assessuniq.'" title="'.$meta['title'].'">
     <qtimetadata>
       <qtimetadatafield>
         <fieldlabel>cc_maxattempts</fieldlabel>
         <fieldentry>1</fieldentry>
-      </qtimetadatafield>
-      <qtimetadatafield>
-        <fieldlabel>keyword</fieldlabel>
-        <fieldentry>'.$keywd.'</fieldentry>
-      </qtimetadatafield>
-    </qtimetadata>
+      </qtimetadatafield>';
+    if (isset($meta['book'])) {
+    	    $c .= ' <qtimetadatafield>
+        <fieldlabel>qmd_publication</fieldlabel>
+        <fieldentry>'.$meta['book'].'</fieldentry>
+      </qtimetadatafield>';
+     }
+     if (isset($meta['org'])) {
+    	    $c .= ' <qtimetadatafield>
+        <fieldlabel>qmd_organization</fieldlabel>
+        <fieldentry>'.$meta['org'].'</fieldentry>
+      </qtimetadatafield>';
+     } 
+     if (isset($meta['author'])) {
+    	    $c .= ' <qtimetadatafield>
+        <fieldlabel>qmd_author</fieldlabel>
+        <fieldentry>'.$meta['author'].'</fieldentry>
+      </qtimetadatafield>';
+     }
+     if (isset($meta['chapter'])) {
+    	    $c .= ' <qtimetadatafield>
+        <fieldlabel>qmd_chapter</fieldlabel>
+        <fieldentry>'.$meta['chapter'].'</fieldentry>
+      </qtimetadatafield>';
+     }
+     if (isset($meta['chpn'])) {
+    	    $c .= ' <qtimetadatafield>
+        <fieldlabel>qmd_chapter_number</fieldlabel>
+        <fieldentry>'.$meta['chpn'].'</fieldentry>
+      </qtimetadatafield>';
+     }
+     if (isset($meta['license'])) {
+    	    $c .= ' <qtimetadatafield>
+        <fieldlabel>qmd_license</fieldlabel>
+        <fieldentry>'.$meta['license'].'</fieldentry>
+      </qtimetadatafield>';
+     }
+     if (isset($meta['licenseurl'])) {
+    	    $c .= ' <qtimetadatafield>
+        <fieldlabel>qmd_license_id</fieldlabel>
+        <fieldentry>'.$meta['licenseurl'].'</fieldentry>
+      </qtimetadatafield>';
+     }
+     
+    $c .=' </qtimetadata>
     <section ident="root_section">';
     return $c;
 }
@@ -45,12 +96,17 @@ $zip->open($dir.'/'.$dir.".zip",ZipArchive::OVERWRITE);
 $n = 0;
 foreach ($files as $file) {
 	$chp = explode('.',basename($file))[0];
-	if ($filelevel == "chapter") {
-		$out = startqti($chp,$keywords.' Chapter '.substr($chp,2));
-	}
-
+	
 	$html = file_get_contents($file);
 	phpQuery::newDocumentHTML($html);
+	$meta['chapter'] = pq("h1.title span.cnx-gentext-t")->text();
+	$meta['chpn'] = pq("h1.title span.cnx-gentext-n")->text();
+	
+	if ($filelevel == "chapter") {
+		$meta['title'] = $titleprefix." Chapter ".$meta['chpn'];
+		$out = startqti();
+	}
+	
 	//.multiple-choice is used in bio, .section-quiz in soc
 	$quiz = pq(".cnx-eoc.multiple-choice, .cnx-eoc.section-quiz");
 	$secs = pq($quiz)->find(".section");
@@ -60,7 +116,10 @@ foreach ($files as $file) {
 		$sectitle = pq($sec)->find(".cnx-gentext-section.cnx-gentext-n")->text();
 		
 		if ($filelevel == "section") {
-			$out = startqti("Section".$sectitle,$keywords.' Section '.$sectitle);
+			$meta['chapter'] = rtrim(pq($sec)->find(".cnx-gentext-section.cnx-gentext-t")->text(), " *");
+			$meta['chpn'] = $sectitle;
+			$meta['title'] = $titleprefix." Section ".$sectitle;
+			$out = startqti();
 		}
 		
 		//it appears we don't need to be this specific
@@ -69,7 +128,8 @@ foreach ($files as $file) {
 		foreach ($mcs as $c=>$mc) {
 			$title = $sectitle.' '. pq($mc)->find(".title:first")->text();
 			if ($filelevel == "question") {
-				$out = startqti("Section".$sec.'#'.$c,$keywords.' Section '.$title);
+				$meta['title'] = $titleprefix." Section ".$sectitle.'#'.$c;
+				$out = startqti();
 			}
 			
 			$prob = pq($mc)->find(".problem");
@@ -91,7 +151,7 @@ foreach ($files as $file) {
 			$soln = array_search($solnval, $lets);
 			$corrects = array($soln);
 			
-			$out .= '<item ident="'.$chp.$secnum.'q'.$c.'" title="'.($title).'">
+			$out .= '<item ident="'.$assessuniq.'q'.$n.'" title="'.($title).'">
 		<itemmetadata>
 		  <qtimetadata>
 		    <qtimetadatafield>
@@ -107,14 +167,14 @@ foreach ($files as $file) {
 		</itemmetadata>
 		<presentation>
 		  <material>
-		    <mattext texttype="text/html">'.trim($prompt).'</mattext>
+		    <mattext texttype="text/html">'.htmlentities(trim($prompt),ENT_XML1).'</mattext>
 		  </material>
 		  <response_lid ident="response1" rcardinality="Single">
 		    <render_choice>';
 		    foreach ($solntext as $k=>$it) {
-			    $out .= '<response_label ident="'.$chp.$secnum.'q'.$c.'o'.$k.'">
+			    $out .= '<response_label ident="'.$assessuniq.'q'.$n.'o'.$k.'">
 			<material>
-			  <mattext texttype="text/html">'.trim($it).'</mattext>
+			  <mattext texttype="text/html">'.htmlentities(trim($it),ENT_XML1).'</mattext>
 			</material>
 		      </response_label>';
 		   }
@@ -130,14 +190,14 @@ foreach ($files as $file) {
 		    <conditionvar>
 		    ';
 		    if (count($corrects)==1) {
-			    $out .= '<varequal respident="response1">'.$chp.$secnum.'q'.$c.'o'.$corrects[0].'</varequal>';
+			    $out .= '<varequal respident="response1">'.$assessuniq.'q'.$n.'o'.$corrects[0].'</varequal>';
 		    } else {
 			    $out .= '<and>';
 			    foreach ($solntext as $k=>$it) {
 				    if (in_array($k,$corrects)) {
-					    $out .= '<varequal respident="response1">'.$chp.$secnum.'q'.$c.'o'.$k.'</varequal>';
+					    $out .= '<varequal respident="response1">'.$assessuniq.'q'.$n.'o'.$k.'</varequal>';
 				    } else {
-					    $out .= '<not><varequal respident="response1">'.$chp.$secnum.'q'.$c.'o'.$k.'</varequal></not>';
+					    $out .= '<not><varequal respident="response1">'.$assessuniq.'q'.$n.'o'.$k.'</varequal></not>';
 				    }
 			    }
 			    $out .= '</and>';
@@ -153,8 +213,8 @@ foreach ($files as $file) {
 	      			$out .= endqti();
 	      			$zip->addFromString($sectitle.'.'.$c.".xml", $out);
 	      		}
+	      		$n++;
 		}
-		$n+=count($mcs);
 		if ($filelevel == "section" && count($mcs)>0) {
 			$out .= endqti();
 			$zip->addFromString($sectitle.".xml", $out);
